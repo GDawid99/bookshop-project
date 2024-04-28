9b08ce5bdc5799fb139b2f2036e8b3c21fbc2ab8
unknown <gabrysdaw@gmail.com>
2024-04-28 21:20:47 +0200

    adding a star rating

--- frontend/bookshop/src/components/RecommendedBook.js
+++ frontend/bookshop/src/components/RecommendedBook.js
@@ -1,4 +1,4 @@
-import { Button, Typography } from "@mui/material";
+import { Button, Rating, Typography } from "@mui/material";
 
 
 export const RecommendedBook = (props) => {
@@ -11,6 +11,9 @@ export const RecommendedBook = (props) => {
             <Typography variant="h6" sx={{fontSize:"9px"}}>Wydawnictwo: {props.publisher}</Typography>
             <Typography variant="h6" display="inline" sx={{fontSize:"9px"}}>Cena: </Typography>
             <Typography variant="h6" display="inline" sx={{fontSize:"9px", color:"purple"}}>{props.price} zł</Typography>
+            <Typography>
+                <Rating value={props.rating/2} readOnly precision={0.5} size="small"></Rating>
+            </Typography>
             <Typography>
                 <Button variant="h5" display="flex">SPRAWDŹ</Button>
             </Typography>
