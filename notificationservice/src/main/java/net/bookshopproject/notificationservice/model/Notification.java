512f75ec61f0e378b4f1558e76a61bd22332e75a
unknown <gabrysdaw@gmail.com>
2024-05-30 21:55:47 +0200

    add timetolive

--- notificationservice/src/main/java/net/bookshopproject/notificationservice/model/Notification.java
+++ notificationservice/src/main/java/net/bookshopproject/notificationservice/model/Notification.java
@@ -6,20 +6,24 @@ import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
 @Getter
 @Setter
 @ToString
-@RedisHash("notification")
+@RedisHash(value = "notification", timeToLive = 15L)
 public class Notification {
     @Id
+    @Indexed
     private String id;
     private String cartId;
     @Indexed
     private String userId;
     private String title;
     private String body;
+    @TimeToLive
+    private Long expiration;
 
     public Notification(String cartId, String userId, String title, String body) {
         this.cartId = cartId;
